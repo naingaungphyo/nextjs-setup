1bd84b258afea70160f3e49a9b2f4bcf7afa7b71
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-07-20 17:04:02 +0900

    custom component folder

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const indexPage = () => {
   return (
     <div>
       <h1>The Main Page</h1>
+      <User name="Naing" age="27"></User>
     </div>
   );
 };
